ae54004bf6acf9bef89a01a2c93e79efb9ab8eb7
ebi63 <ebi.mohamadi63@yahoo.com>
2024-10-10 06:12:14 -0700

    dictionary is added

--- robotech/khayam_pasgal_with_lists.py
+++ robotech/khayam_pasgal_with_lists.py
@@ -1,13 +1,12 @@
-
-previous_row = [1, 1]
-
-n_rows = 5
-print([1])
-for _ in range(n_rows-1):
-    rows = [1, 1]
-    print(previous_row)
-    for j in range(1, len(previous_row)):
-        rows.insert(j, previous_row[j] + previous_row[j-1])
+def khayam_list(n_rows):
+    previous_row = [1, 1]
+    print([1])
+    for _ in range(n_rows-1):
+        rows = [1, 1]
+        print(previous_row)
+        for j in range(1, len(previous_row)):
+            rows.insert(j, previous_row[j] + previous_row[j-1])
+        
+        previous_row = rows 
     
-    previous_row = rows 
-    
+khayam_list(5)
---
+++ robotech/week5/dic.py
@@ -0,0 +1,19 @@
+
+"""
+# dictionary methods
+
+dic_name.get(value1, value2)
+
+
+""" 
+programming_languag = {"v":1, "c":2}
+
+for k, v in programming_languag.items():
+    pass
+
+for k in programming_languag.keys():
+    pass
+
+for v in programming_languag.values():
+    pass
+
